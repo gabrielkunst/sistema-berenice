91dbe597e685480e9a5c20fd10e0e6f0b08753d4
Gabriel Kunst <118089848+gabrielkunst@users.noreply.github.com>
2023-05-04 11:29:16 -0300

    adicionei mais algumas funções da v2

--- PROJETO 3/mainGabriel.c
+++ PROJETO 3/mainGabriel.c
@@ -8,8 +8,10 @@ typedef struct {
     double preco;
     int estoque;
     int vendidos;
-} Product;
+    int tempVendidos
+} Produto;
 
+/* FUNÇÕES GERAIS */
 /* MOSTRA ERRO */
 void mostrarErro(char message[]) {
     printf("%s\n", message);
@@ -18,8 +20,46 @@ void mostrarErro(char message[]) {
     }
 }
 
+/* FUNCAO RESPONSAVEL POR ORGANIZAR A LISTA DE MAIOR SUBTOTAL PARA MENOR */
+void organizarListaSubtotal(Produto produtos[], int tamanho) {
+    Produto temp;
+    for (int i = 0; i < tamanho; i++) {
+        for (int j = i + 1; j < tamanho; j++) {
+            if ((produtos[i].preco * produtos[i].vendidos) < (produtos[j].preco * produtos[j].vendidos)) {
+                temp = produtos[i];
+                produtos[i] = produtos[j];
+                produtos[j] = temp;
+            }
+        }
+    }
+}
+
+/* FUNCAO RESPONSAVEL POR ORGANIZAR A LISTA DO MENOR ID PARA O MAIOR ID */
+void organizarListaId(Produto produtos[], int tamanho) {
+    Produto temp;
+    for (int i = 0; i < tamanho; i++) {
+        for (int j = i + 1; j < tamanho; j++) {
+            if (produtos[i].id > produtos[j].id) {
+                temp = produtos[i];
+                produtos[i] = produtos[j];
+                produtos[j] = temp;
+            }
+        }
+    }
+}
+
+/* FUNCAO PARA RESETAR AS VENDAS POS PAGAMENTO */
+void resetarVendas(Produto produtos[], int tamanho, float *total) {
+    for (int i = 0; i < tamanho; i++) {
+        produtos[i].vendidos += produtos[i].tempVendidos;
+        produtos[i].tempVendidos = 0;
+    }
+    *total = 0;
+}
+
+/* FUNÇÕES DOS PRODUTOS --------------------------------------------------------*/
 /* REMOVE '_' DO NOME DO PRODUTO AO DAR FETCH NO ARQUIVO */
-void removerUnderscore(Product produtos[], int tamanho) {
+void removerUnderscore(Produto produtos[], int tamanho) {
     for (int i = 0; i < tamanho; i++) {
         char *nome = produtos[i].nome;
         for (int j = 0; nome[j] != '\0'; j++) {
@@ -31,7 +71,7 @@ void removerUnderscore(Product produtos[], int tamanho) {
 }
 
 /* BUSCA OS PRODUTOS NO ARQUIVO TXT */
-void fetchProdutos(Product **produtos, int *tamanho) {
+void fetchProdutos(Produto **produtos, int *tamanho) {
     FILE *file = fopen("C:\\Users\\GK\\Documents\\GitHub\\sistema-berenice\\TESTES\\produtos.txt", "r");
     if (file == NULL) {
         printf("\nErro na leitura do arquivo!\n");
@@ -39,13 +79,14 @@ void fetchProdutos(Product **produtos, int *tamanho) {
         exit(0);
     }
     fscanf(file, "%d", tamanho);
-    *produtos = malloc(*tamanho * sizeof(Product));
+    *produtos = malloc(*tamanho * sizeof(Produto));
     for (int i = 0; i < *tamanho; i++) {
         fscanf(file, "%lf", &(*produtos)[i].id);
         fscanf(file, "%s", (*produtos)[i].nome);
         fscanf(file, "%lf", &(*produtos)[i].preco);
         fscanf(file, "%d", &(*produtos)[i].estoque);
         fscanf(file, "%d", &(*produtos)[i].vendidos);
+        (*produtos)[i].tempVendidos = 0;
     }
     fclose(file);
     file = NULL;
@@ -53,8 +94,8 @@ void fetchProdutos(Product **produtos, int *tamanho) {
 }
 
 /* SALVA A LISTA ATUAL NO ARQUIVO TXT */
-void salvarProdutos(Product produtos[], int tamanho) {
-    Product tempProdutos[tamanho];
+void salvarProdutos(Produto produtos[], int tamanho) {
+    Produto tempProdutos[tamanho];
     for (int i = 0; i < tamanho; i++) {
         tempProdutos[i] = produtos[i];
         for (int j = 0; tempProdutos[i].nome[j] != '\0'; j++) {
@@ -79,10 +120,12 @@ void salvarProdutos(Product produtos[], int tamanho) {
     }
     fclose(file);
     file = NULL;
+    printf("\nSalvando produtos ...\n");
+    printf("Dados salvos com sucesso!\n");
 }
 
 /* MOSTRA A TABELA NA TELA */
-void printarTabelaProdutos(Product produtos[], int tamanho) {
+void printarTabelaProdutos(Produto produtos[], int tamanho) {
     printf("\n-----------------------------------------------------------------\n");
     printf("|                           PRODUTOS                            |\n");
     printf("-----------------------------------------------------------------\n");
@@ -95,19 +138,37 @@ void printarTabelaProdutos(Product produtos[], int tamanho) {
 }
 
 /* CADASTRA NOVOS PRODUTOS */
-void cadastrarProdutos(Product **produtos, int *tamanho) {
+void cadastrarProdutos(Produto **produtos, int *tamanho) {
     int quantidadeParaCadastar = 0;
     printf("Quantidade de produtos a serem cadastrados: ");
     scanf("%d", &quantidadeParaCadastar);
-    *produtos = realloc(*produtos, (*tamanho + quantidadeParaCadastar) * sizeof(Product *));
+    *produtos = realloc(*produtos, (*tamanho + quantidadeParaCadastar) * sizeof(Produto *));
     for (int i = *tamanho; i < (*tamanho + quantidadeParaCadastar); i++) {
         printf("Digite as informacoes para o produto %d: \n", i + 1);
     }
     //*tamanho += quantidadeParaCadastar;
 }
 
+/* FUNÇÕES DE VENDAS -------------------------------------- */
+/*FUNCAO RESPONSAVEL PELA RELATORIO DE VENDAS */
+void mostrarRelatorioDeVendas(Produto produtos[], int tamanho, float total) {
+    organizarListaId(produtos, tamanho);
+    printf("\n-------------------------------------------------------------------------------------------------\n");
+    printf("|                                  RELATORIO DE VENDAS                                          |\n");
+    printf("-------------------------------------------------------------------------------------------------\n");
+    printf("|%-12s|%-17s|%-19s|%-15s|%-15s\t|\n", "CODIGO", "ITEM", "VALOR TOTAL POR UN.", "ESTOQUE FINAL", "QUANTIDADE VENDIDA POR UN.");
+    printf("-------------------------------------------------------------------------------------------------\n");
+    for (int i = 0; i < tamanho; i++) {
+        printf("|%-12.0lf|%-17s|R$%-17.2lf|%-15d|%-26d\t|\n", produtos[i].id, produtos[i].nome, produtos[i].preco * produtos[i].vendidos, produtos[i].estoque, produtos[i].vendidos);
+    }
+    printf("-------------------------------------------------------------------------------------------------\n");
+    printf("|%30s|%-35s|R$%-18.2f\t|\n", "", "VALOR TOTAL VENDIDO", total);
+    printf("-------------------------------------------------------------------------------------------------\n");
+}
+
+/* FUNÇÕES PRINCIPAIS */
 /* FUNCAO RESPONSAVEL PELA OPCAO 1, PRODUTOS */
-void abrirProdutos() {
+void abrirProdutos(Produto *produtos, int *tamanho) {
     int opcaoMenu = 0;
     while (opcaoMenu != 7) {
         printf("\n|     PRODUTOS    |\n");
@@ -117,7 +178,7 @@ void abrirProdutos() {
         } else {
             switch (opcaoMenu) {
                 case 1:
-                    printf("FUNCAO EXIBIR\n");
+                    printarTabelaProdutos(produtos, *tamanho);
                     break;
                 case 2:
                     printf("FUNCAO CADASTRAR\n");
@@ -129,7 +190,7 @@ void abrirProdutos() {
                     printf("FUNCAO EXCLUIR\n");
                     break;
                 case 5:
-                    printf("FUNCAO SALVAR\n");
+                    salvarProdutos(produtos, *tamanho);
                     break;
                 case 6:
                     printf("FUNCAO LER\n");
@@ -143,23 +204,24 @@ void abrirProdutos() {
 }
 
 /* FUNCAO RESPONSAVEL PELA OPCAO 2, VENDAS */
-void abrirVendas() {
+void abrirVendas(Produto *produtos, int *tamanho) {
     int opcaoMenu = 0;
-    while (opcaoMenu != 7) {
-        printf("\n|     PRODUTOS    |\n");
+    float total = 0, *ptotal = &total;
+    while (opcaoMenu != 3) {
+        printf("\n|     VENDAS    |\n");
         printf("[1] Realizar Venda\n[2] Relatorio de Vendas\n[3] Voltar\nDigite um valor: ");
         if (scanf("%d", &opcaoMenu) != 1 || opcaoMenu < 1 || opcaoMenu > 7) {
             mostrarErro("Valor invalido");
         } else {
             switch (opcaoMenu) {
                 case 1:
-                    printf("FUNCAO REALIZAR VENDA\n");
+                    printf("REALIZAR VENDA\n");
                     break;
                 case 2:
-                    printf("FUNCAO MOSTRAR RELATORIO DE VENDAS\n");
+                    mostrarRelatorioDeVendas(produtos, *tamanho, total);
                     break;
                 case 3:
-                    printf("VOLTANDO AO MENU...\n");
+                    printf("VOLTANDO AO MENU ...\n");
                     break;
             }
         }
@@ -171,9 +233,10 @@ void fecharAplicativo() {
     exit(1);
 }
 
+/* FUNÇÃO PRINCIPAL */
 int main() {
     int tamanho = 0, *ptamanho = &tamanho, opcaoMenu = 0;
-    Product *produtos = NULL;
+    Produto *produtos = NULL;
     fetchProdutos(&produtos, ptamanho);
     while (1) {
         printf("\n|     MENU    |\n");
@@ -183,12 +246,13 @@ int main() {
         } else {
             switch (opcaoMenu) {
                 case 1:
-                    abrirProdutos();
+                    abrirProdutos(produtos, ptamanho);
                     break;
                 case 2:
-                    printf("CHAMAR FUNCAO DE VENDAS\n");
+                    abrirVendas(produtos, ptamanho);
                     break;
                 case 3:
+                    printf("SAINDO ...");
                     fecharAplicativo();
                     break;
             }
