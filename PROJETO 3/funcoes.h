bcf7c468877bbeebb06fef09af76abd2f68b1f2b
Gabriel Kunst <118089848+gabrielkunst@users.noreply.github.com>
2023-05-21 18:15:09 -0300

    projeto 3 finalizado

---
+++ PROJETO 3/funcoes.h
@@ -0,0 +1,63 @@
+#ifndef FUNCOES_H
+#define FUNCOES_H
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#define MAX_NAME_LENGTH 26
+
+typedef struct {
+    double id;
+    char nome[MAX_NAME_LENGTH];
+    double preco;
+    int estoque;
+    int vendidos;
+    int tempVendidos;
+} Produto;
+
+void mostrarErro(char message[]);
+
+bool exiteAlgumProduto(int tamanho);
+
+void organizarListaSubtotal(Produto produtos[], int tamanho);
+
+void organizarListaId(Produto produtos[], int tamanho);
+
+void lerProdutosDoArquivo(Produto** produtos, int* tamanho);
+
+void resetarVendas(Produto produtos[], int tamanho);
+
+void salvarProdutos(Produto produtos[], int tamanho);
+
+void printarTabelaProdutos(Produto produtos[], int tamanho);
+
+int verificarSeIdExiste(Produto produtos[], int tamanho, double idRecebido);
+
+bool temApenasEspacos(char* nomeProduto);
+
+void cadastrarProdutos(Produto** produtos, int* tamanho);
+
+void mostrarRelatorioDeVendas(Produto produtos[], int tamanho, float* total);
+
+void mostrarNotaFiscal(Produto produtos[], int tamanho, float total);
+
+void pagarCompra(Produto produtos[], int tamanho, float totalTemp, float* total);
+
+bool verificarEstoque(Produto produtos[], int tamanho);
+
+bool validarID(Produto produtos[], int tamanho,double id, int *pIndexProduto);
+
+void atualizarProdutos(Produto produtos[], int tamanho);
+
+void deletarERelerArquivo(Produto** produtos, int* tamanho);
+
+void excluirProdutos(Produto *produtos, int *tamanho);
+
+void realizarVenda(Produto produtos[], int tamanho, float* total);
+
+void abrirSubmenuProdutos(Produto* produtos, int* tamanho);
+
+void fecharAplicativo(Produto* produtos);
+
+#endif
